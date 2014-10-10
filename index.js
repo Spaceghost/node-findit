2e16e749675f20559a840cc9b3efa86ef674e3e9
Andrew Kelley <superjoe30@gmail.com>
2014-10-10 11:38:00 -0700

    leaner, cleaner findit
    fixes #22
    fixes #25
    fixes #29
    fixes #32
    fixes #33

--- index.js
+++ index.js
@@ -1,187 +1,110 @@
 var EventEmitter = require('events').EventEmitter;
-var rfs = require('fs');
+var fs = require('fs');
 var path = require('path');
 
-module.exports = function walk (dir, opts, emitter, dstat) {
-    if (!opts) opts = {};
-    var fdir = opts._original || dir;
-    opts._original = undefined;
-    var fs = opts.fs || rfs;
-    
-    if (!emitter) {
-        emitter = new EventEmitter;
-        emitter.stop = function () {
-            emitter._stopped = true;
-            emitter.emit('stop');
-        };
-        emitter._pending = 0;
-        emitter._seen = {};
+module.exports = findit;
+
+function findit(basedir, opts) {
+  opts = opts || {};
+  var followSymlinks = !!opts.followSymlinks;
+  var myFs = opts.fs || fs;
+  var emitter = new EventEmitter();
+  var stopped = false;
+  var pending = 0;
+  var seen = {};
+
+  emitter.stop = stop;
+  walkPath(basedir);
+  return emitter;
+
+  function recursiveReadDir(basedir) {
+    pendStart();
+    myFs.readdir(basedir, function(err, entries) {
+      if (stopped) return;
+      if (err) {
+        handleError(err, basedir);
+        pendEnd();
+        return;
+      }
+      entries.forEach(function(entry) {
+        walkPath(path.join(basedir, entry));
+      });
+      pendEnd();
+    });
+  }
+
+  function walkPath(fullPath) {
+    if (seen[fullPath]) {
+      var err = new Error("file system loop detected");
+      err.code = 'ELOOP';
+      handleError(err, fullPath);
+      return;
     }
-    
-    if (dstat) {
-        var stopped = false;
-        emitter.emit('directory', fdir, dstat, function stop () {
-            stopped = true;
-        });
-        emitter.emit('path', fdir, dstat);
-        if (!stopped) {
-            emitter._pending ++;
-            fs.readdir(dir, function (err, files) {
-                emitter._pending --;
-                onreaddir(dir, err, files);
-            });
-        }
-        else check()
+    seen[fullPath] = true;
+
+    pendStart();
+    myFs.lstat(fullPath, function(err, stats) {
+      if (stopped) return;
+      if (err) {
+        handleError(err, fullPath);
+        pendEnd();
+        return;
+      }
+      emitter.emit('path', fullPath, stats);
+      if (stats.isDirectory()) {
+        emitter.emit('directory', fullPath, stats, stop);
+        recursiveReadDir(fullPath);
+      } else if (stats.isFile()) {
+        emitter.emit('file', fullPath, stats);
+      } else if (stats.isSymbolicLink()) {
+        emitter.emit('link', fullPath, stats);
+        if (followSymlinks) recursiveReadLink(fullPath);
+      }
+      pendEnd();
+    });
+  }
+
+  function recursiveReadLink(fullPath) {
+    pendStart();
+    myFs.readlink(fullPath, function(err, linkString) {
+      if (stopped) return;
+      if (err) {
+        handleError(err, fullPath);
+        pendEnd();
+        return;
+      }
+      var linkPath = path.join(path.dirname(fullPath), linkString);
+      emitter.emit('readlink', fullPath, linkPath);
+      walkPath(linkPath);
+      pendEnd();
+    });
+  }
+
+  function stop() {
+    if (stopped) return;
+    stopped = true;
+    emitter.emit('stop');
+  }
+
+  function handleError(err, errPath) {
+    if (!err || stopped) return;
+    err.path = errPath;
+    emitter.emit('error', err);
+  }
+
+  function pendStart() {
+    pending += 1;
+  }
+
+  function pendEnd() {
+    if (stopped) return;
+    pending -= 1;
+    if (pending === 0) {
+      emitter.emit('end');
+    } else if (pending < 0) {
+      // this should never happen; if this gets thrown we need to debug findit
+      // and this stack trace will help.
+      throw new Error("pendEnd called too many times");
     }
-    else {
-        emitter._pending ++;
-        fs.lstat(dir, function (err, stat) {
-            emitter._pending --;
-            onlstat(err, stat);
-        });
-    }
-    function onlstat (err, stat) {
-        if (emitter._stopped) return;
-        
-        if (err) {
-            if (!err.path) err.path = dir;
-            emitter.emit('error', err);
-            return check();
-        }
-        emitter._seen[stat.ino || dir] = true;
-        
-        if (stat.isSymbolicLink() && opts.followSymlinks) {
-            emitter.emit('link', fdir, stat);
-            emitter._pending ++;
-            fs.readlink(dir, function (err, rfile) {
-                if (emitter._stopped) return;
-                emitter._pending --;
-                if (err) {
-                    if (!err.path) err.path = dir;
-                    emitter.emit('error', err);
-                    return check();
-                }
-                var file_ = path.resolve(dir, rfile);
-                emitter.emit('readlink', fdir, file_);
-                emitter._pending ++;
-                fs.lstat(file_, function (err, stat) {
-                    emitter._pending --;
-                    onstat(err, stat);
-                });
-            });
-        }
-        else if (stat.isSymbolicLink()) {
-            emitter.emit('link', fdir, stat);
-            emitter.emit('path', fdir, stat);
-            check();
-        }
-        else if (stat.isDirectory()) {
-            var stopped = false;
-            emitter.emit('directory', fdir, stat, function stop () {
-                stopped = true;
-            });
-            emitter.emit('path', fdir, stat);
-            if (!stopped) {
-                emitter._pending ++;
-                fs.readdir(dir, function (err, files) {
-                    emitter._pending --;
-                    onreaddir(dir, err, files);
-                });
-            }
-            else check()
-        }
-        else {
-            emitter.emit('file', fdir, stat);
-            emitter.emit('path', fdir, stat);
-            check();
-        }
-    }
-    
-    return emitter;
-    
-    function check () {
-        if (emitter._pending === 0) finish();
-    }
-    
-    function finish () {
-        emitter.emit('end');
-        emitter._stopped = true;
-    }
-    
-    function onreaddir (dir, err, files) {
-        if (emitter._stopped) return;
-        
-        if (err) {
-            if (!err.path) err.path = dir;
-            emitter.emit('error', err);
-            return check();
-        }
-        
-        files.forEach(function (rfile) {
-            var file = path.join(fdir, rfile);
-            
-            emitter._pending ++;
-            fs.lstat(file, function (err, stat) {
-                emitter._pending --;
-                
-                if (emitter._stopped) return;
-                if (err) {
-                    if (!err.path) err.path = file;
-                    emitter.emit('error', err);
-                    check()
-                }
-                else onstat(file, stat)
-            });
-        });
-       check();
-    }
-    
-    function onstat (file, stat, original) {
-        if (emitter._seen[stat.ino || file]) return check();
-        emitter._seen[stat.ino || file] = true;
-        
-        if (stat.isDirectory()) {
-            if (original) opts._original = original;
-            walk(file, opts, emitter, stat);
-            check();
-        }
-        else if (stat.isSymbolicLink() && opts.followSymlinks) {
-            emitter.emit('link', file, stat);
-            
-            fs.readlink(file, function (err, rfile) {
-                if (emitter._stopped) return;
-                if (err) {
-                    if (!err.path) err.path = file;
-                    emitter.emit('error', err);
-                    return check();
-                }
-                var file_ = path.resolve(path.dirname(file), rfile);
-                
-                emitter.emit('readlink', file, file_);
-                emitter._pending ++;
-                fs.lstat(file_, function (err, stat_) {
-                    emitter._pending --;
-                    if (emitter._stopped) return;
-                    if (err) {
-                        if (!err.path) err.path = file_;
-                        emitter.emit('error', err);
-                        return check();
-                    }
-                    
-                    onstat(file_, stat_, file);
-                    check();
-                });
-            });
-        }
-        else if (stat.isSymbolicLink()) {
-            emitter.emit('link', file, stat);
-            emitter.emit('path', file, stat);
-            check();
-        }
-        else {
-            emitter.emit('file', file, stat);
-            emitter.emit('path', file, stat);
-            check();
-        }
-    }
-};
+  }
+}
